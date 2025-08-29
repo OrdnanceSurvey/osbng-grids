c0ab595a5cbb2e4f880e3f094c142e288870b484
Steve Kingston <steve.kingston@os.uk>
2025-08-29 18:04:30 +0100

    chore: format and lint create_osbng_grids.py using ruff

--- osbng-grids/scripts/create_osbng_grids.py
+++ osbng-grids/scripts/create_osbng_grids.py
@@ -1,22 +1,24 @@
-"""Create British National Grid (BNG) grid square data at a range of resolutions covering the BNG index system bounds.
+"""Create BNG grid square data at multiple resolutions over the BNG index system bounds.
 
-Data generation uses the __geo_interface__ (https://gist.github.com/sgillies/2217756) representation of BNG grid squares, 
-a GeoJSON-like mapping which enables integration with the GeoPandas (https://github.com/geopandas/geopandas) Python package.
+Data generation uses the __geo_interface__ (https://gist.github.com/sgillies/2217756) 
+representation of BNG grid squares, a GeoJSON-like mapping which enables integration 
+with the GeoPandas (https://github.com/geopandas/geopandas) Python package.
 
-Data saved as GeoParquet (https://github.com/opengeospatial/geoparquet) and GeoPackage (GPKG) at 100km, 50km, 10km, 5km and 1km resolutions.
+Data saved as GeoParquet (https://github.com/opengeospatial/geoparquet) and GeoPackage 
+(GPKG) at 100km, 50km, 10km, 5km and 1km resolutions.
 """
 
 import geopandas as gpd
-
 from osbng.grids import (
-    bng_grid_100km,
-    bng_grid_50km,
-    bng_grid_10km,
-    bng_grid_5km,
     bng_grid_1km,
+    bng_grid_5km,
+    bng_grid_10km,
+    bng_grid_50km,
+    bng_grid_100km,
 )
 
-# Grid square data covering the BNG index system bounds at 100km, 50km, 10km, 5km and 1km resolutions
+# Grid square data covering the BNG index system bounds at 100km, 50km, 10km, 5km 
+# and 1km resolutions
 # Provided as iterators of GeoJSON-like mappings using the __geo_interface__ protocol
 # Datasets
 datasets = [bng_grid_100km, bng_grid_50km, bng_grid_10km, bng_grid_5km, bng_grid_1km]
@@ -25,7 +27,6 @@ resolutions = ["100km", "50km", "10km", "5km", "1km"]
 
 # Save each dataset as GeoParquet and GPKG
 for dataset, resolution in zip(datasets, resolutions):
-
     # Create GeoPandas GeoDataFrame from each dataset
     # Set GeoDataFrame coordinate reference system to British National Grid (EPSG:27700)
     # https://epsg.io/27700
